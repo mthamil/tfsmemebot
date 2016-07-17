6de58eca5e08cc070c2b6ac84c4bd9ee5e5110ed
Matthew Hamilton <matthamilton@live.com>
2016-07-17 13:06:49 -0500

    Set up repository.

---
+++ README.md
@@ -0,0 +1,6 @@
+TFS Meme Bot
+=================
+
+Copyright (C) 2016 Matt Hamilton
+
+MemeBot is a web service that integrates with TFS webhooks to provide a chat bot with meme functions.
---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp     from "gulp";
+import del      from "del";
+import babel    from "gulp-babel";
+
+const config = {
+    in: "src/",
+    out: "dist/"
+};
+
+export const clean = done => 
+    del([`${config.out}*`], done);
+
+export const source = () =>
+    gulp.src([`${config.in}**/*.js`], { base: `${config.in}` })
+        .pipe(babel())
+        .pipe(gulp.dest(config.out));
+
+export const content = gulp.series(() =>
+    gulp.src([`${config.in}**`, `!${config.in}**/*.js`], { base: `${config.in}` })
+        .pipe(gulp.dest(config.out)));
+
+export const build = gulp.series(clean, source, content);
+
+export default build;
