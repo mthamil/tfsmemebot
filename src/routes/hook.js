3dc830475e8daafa1f3cb70f5e33f3e29f28f0db
Matthew Hamilton <matthamilton@live.com>
2016-07-18 04:21:01 -0500

    Use forever instead of nodemon which kept crashing.

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -3,7 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/", (req, res) => {
-  res.send("Hello World!");
+  const data = JSON.parse(req.body);
+  res.json(data);
 });
 
 export { router };
