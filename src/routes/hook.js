ab494b9fe3e0831454ba6a48fe0be71ea5b77ddb
Matthew Hamilton <matthamilton@live.com>
2016-07-20 02:15:50 -0500

    Refactoring and code cleanup.

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -4,29 +4,28 @@ import { parser }        from "../services/command-parser";
 import { ChatService }   from "../services/chat";
 
 const router = Router();
-const chat = new ChatService();
 
 router.post("/retrieve", (req, res) => {
+    const memeGen = new MemeGenerator();
+    const chat = new ChatService();
+    const message = req.body;
 
-  const message = req.body;
-  if (message.eventType && message.eventType === "message.posted" &&
-      message.publisherId && message.publisherId === "tfs" &&
-      message.resource && message.resource.content) {
+    if (message.eventType && message.eventType === "message.posted" &&
+        message.publisherId && message.publisherId === "tfs" &&
+        message.resource && message.resource.content) {
 
-        const command = message.resource.content;
-        const result = parser.parse(command);
-        if (result) {
-          new MemeGenerator()
-            .create(result.name, result.topText, result.bottomText)
-            .then(imageUrl => {
-                  return chat.send(message.resource.postedRoomId, imageUrl)
-            })
-            .catch(error => res.send(error));
-          return;
-        }
-  }
+            const command = message.resource.content;
+            const result = parser.parse(command);
+            if (result) {
+                memeGen
+                  .create(result.name, result.topText, result.bottomText)
+                  .then(imageUrl => chat.send(message.resource.postedRoomId, imageUrl))
+                  .catch(error => res.send(error));
+                return;
+            }
+    }
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 export { router };
--- src/services/chat.js
+++ src/services/chat.js
@@ -1,19 +1,27 @@
-import request from "request-ntlm-continued";
+import request     from "request-ntlm-continued";
 import { Promise } from "bluebird";
-import URITemplate       from "urijs/src/URITemplate";
-import config            from "config";
+import URITemplate from "urijs/src/URITemplate";
+import config      from "config";
 
-const tfsConfig = config.get("TFS");
 const post = Promise.promisify(request.post, { context: request });
 
 class ChatService {
+    constructor() {
+        const tfsConfig = config.get("TFS");
+        this.apiRoot = tfsConfig.apiRoot;
+        this.username = tfsConfig.username;
+        this.password = tfsConfig.password;
+        this.domain = tfsConfig.domain;
+        this.workstation = tfsConfig.workstation;
+    }
+
     send(roomId, message) {
           return post({
-                    url: URITemplate(tfsConfig.apiRoot).expand({ roomId: roomId }).toString(),
-                    username: tfsConfig.username,
-                    password: tfsConfig.password,
-                    ntlm_domain: tfsConfig.domain,
-                    workstation: tfsConfig.workstation
+                    url: URITemplate(this.apiRoot).expand({ roomId }).toString(),
+                    username: this.username,
+                    password: this.password,
+                    ntlm_domain: this.domain,
+                    workstation: this.workstation
                 }, { 
                     content: message 
                 });
