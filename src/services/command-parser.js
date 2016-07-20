e44be9b4df617e249b7cd01e273205b00400f18a
Matthew Hamilton <matthamilton@live.com>
2016-07-20 17:33:03 -0500

    Improve parser robustness and add more tests.

--- src/services/command-parser.js
+++ src/services/command-parser.js
@@ -5,18 +5,21 @@ class CommandParser {
         if (prefixIndex > -1) {
             input = input.substr(prefixIndex + 8);
             const tokens = input.trim().split(";");
-
             const result = { name: "", topText: "", bottomText: "" };
-            if (tokens.length > 1) {
-                result.name = tokens[0].trim();
-                result.topText = tokens[1].trim();
 
-                if (tokens.length > 2) {
-                    result.bottomText = tokens[2].trim();
+            if (tokens.length > 0 && tokens[0].trim() !== "") {
+                result.name = tokens[0].trim();
+
+                if (tokens.length > 1) {
+                    result.topText = tokens[1].trim();
+
+                    if (tokens.length > 2) {
+                        result.bottomText = tokens[2].trim();
+                    }
                 }
 
                 return result;
-            }  
+            }
         }
 
         return null;
--- src/tests/services/command-parser-tests.js
+++ src/tests/services/command-parser-tests.js
@@ -4,6 +4,34 @@ import { parser } from "../../services/command-parser";
 
 describe("command parser tests", () => {
 
+    ["@memebot ",
+     "@memebot"].forEach(input =>
+    {
+        it(`'${input}': should not match`, () => {
+            // Act.
+            const result = parser.parse(input)
+
+            // Assert.
+            assert.isNull(result);
+        });
+    });
+
+    ["@memebot facepalm;",
+     "@memebot facepalm  ",
+     "@memebot    facepalm;"].forEach(input =>
+    {
+        it(`'${input}': should match`, () => {
+            // Act.
+            const result = parser.parse(input)
+
+            // Assert.
+            assert.isNotNull(result);
+            assert.equal(result.name, "facepalm");
+            assert.equal(result.topText, "");
+            assert.equal(result.bottomText, "");
+        });
+    });
+
     ["@memebot facepalm; line1; line 2",
      "@memebot facepalm;line1;line 2",
      "@memebot    facepalm;   line1;    line 2"].forEach(input =>
@@ -13,6 +41,7 @@ describe("command parser tests", () => {
             const result = parser.parse(input)
 
             // Assert.
+            assert.isNotNull(result);
             assert.equal(result.name, "facepalm");
             assert.equal(result.topText, "line1");
             assert.equal(result.bottomText, "line 2");
@@ -28,15 +57,30 @@ describe("command parser tests", () => {
             const result = parser.parse(input)
 
             // Assert.
+            assert.isNotNull(result);
             assert.equal(result.name, "facepalm");
             assert.equal(result.topText, "line1");
             assert.equal(result.bottomText, "");
         });
     });
 
-    ["@memebotfacepalm; line1; line 2",
-     //"@memebot facepalm;    ",
-     "@memebot    facepalm   line1"].forEach(input =>
+    ["@memebot facepalm; ;line 2",
+     "@memebot facepalm;;   line 2;",
+     "@memebot    facepalm; ;  line 2 "].forEach(input =>
+    {
+        it(`'${input}': should match`, () => {
+            // Act.
+            const result = parser.parse(input)
+
+            // Assert.
+            assert.isNotNull(result);
+            assert.equal(result.name, "facepalm");
+            assert.equal(result.topText, "");
+            assert.equal(result.bottomText, "line 2");
+        });
+    });
+
+    ["@memebotfacepalm; line1; line 2"].forEach(input =>
     {
         it(`'${input}': should not match`, () => {
             // Act.
